68558e2c0333956512e0dcb7cf87ae8afc32d1a6
Dhruv Datta <136216533+Dhruv-Datta@users.noreply.github.com>
2023-06-26 09:35:46 -0500

    Create cmd_line.cmd

---
+++ cmd_line.cmd
@@ -0,0 +1,12 @@
+pip install pyinstaller
+
+# Rebrandly Text Integration
+cd C:Path\to\folder\with\script
+
+pyinstaller --onefile --noconsole Rebrandly-Test-Integration.py
+pyinstaller --onefile --noconsole setup.py
+
+# Rebrandly GUI
+cd C:Path\to\folder\with\script
+
+pyinstaller --onefile --noconsole Rebrandly-App-GUI.py
