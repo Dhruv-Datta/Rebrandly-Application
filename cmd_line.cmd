1a67bda5baaeedc4b461c1b2c6eaf76ce1a29ccf
Dhruv Datta <136216533+Dhruv-Datta@users.noreply.github.com>
2023-06-26 13:43:15 -0500

    Update cmd_line.cmd

--- cmd_line.cmd
+++ cmd_line.cmd
@@ -1,12 +1,16 @@
 pip install pyinstaller
 
-# Rebrandly Text Integration
-cd C:Path\to\folder\with\script
-
-pyinstaller --onefile --noconsole Rebrandly-Test-Integration.py
-pyinstaller --onefile --noconsole setup.py
 
 # Rebrandly GUI
 cd C:Path\to\folder\with\script
-
 pyinstaller --onefile --noconsole Rebrandly-App-GUI.py
+
+# Rebrandly Clipboard
+cd C:Path\to\file\Rebrandly-Clipboard.py
+pyinstaller --onefile Rebrandly-App-GUI.py
+
+
+# Rebrandly Text Integration
+cd C:Path\to\folder\with\script
+pyinstaller --onefile --noconsole Rebrandly-Test-Integration.py
+pyinstaller --onefile --noconsole setup.py
